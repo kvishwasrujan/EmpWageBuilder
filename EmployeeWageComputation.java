6a712c5ec435f91e87fdd67cab0b4813ce924982
kvishwasrujan <roohithbandi00@gmail.com>
2020-09-24 21:01:49 +0530

    Added:If else is modified to Switch Case

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,18 +1,26 @@
+
 package empWageBuilder;
 
-public class EmpWageBuilderUC2 {
+public class EmpWageBuilderUC4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int IS_FULL_TIME = 1;
-		int EMP_RATE_PER_HOUR = 20;
+	 final int IS_FULL_TIME = 1;
+	 final int IS_PART_TIME = 2;
+	 final int EMP_RATE_PER_HOUR = 20;
 		int empHrs = 0;
 		int empWage = 0;
-		double empCheck = Math.floor(Math.random()*10)%2;
-		if(empCheck == IS_FULL_TIME)
+		int empCheck = (int) Math.floor(Math.random()*10)%3;
+		switch (empCheck) {
+		case IS_FULL_TIME:
 			empHrs = 8;
-		else
-			empHrs= 0;
+			break;
+		case IS_PART_TIME:
+			empHrs = 4;
+			break;
+	    default:
+	    	empHrs=0;
+		}
 		empWage = empHrs*EMP_RATE_PER_HOUR;
 		System.out.println("Emp Wage: "+ empWage);
 
