8d54e203479328621c4f0402b80cc8f1000a2920
kvishwasrujan <roohithbandi00@gmail.com>
2020-09-24 21:16:13 +0530

    Added: Conditional Monthly Wage functionality added

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,16 +1,18 @@
 package empWageBuilder;
 
-public class EmpWageBuilderUC5 {
+public class EmpWageBuilderUC6 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	 final int IS_FULL_TIME = 1;
 	 final int IS_PART_TIME = 2;
 	 final int EMP_RATE_PER_HOUR = 20;
 	 final int NUM_OF_WORKING_DAYS=20;
+	 final int MAX_HRS_IN_MONTH = 100;
 		int empHrs = 0;
-		int empWage = 0;
-		int totalEmpWage=0;
-		for(int day=0; day<=NUM_OF_WORKING_DAYS; day++) {
+		int totalEmpHrs = 0;
+		int totalWorkingDays=0;
+		while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays< NUM_OF_WORKING_DAYS) {
+			totalWorkingDays++;
 		int empCheck = (int) Math.floor(Math.random()*10)%3;
 		switch (empCheck) {
 		case IS_FULL_TIME:
@@ -22,11 +24,15 @@ public class EmpWageBuilderUC5 {
 	    default:
 	    	empHrs=0;
 		}
-		empWage = empHrs*EMP_RATE_PER_HOUR;
-		totalEmpWage += empWage; 
-		System.out.println("Emp Wage: "+ empWage);
+		totalEmpHrs += empHrs;
+		System.out.println("Day#: "+ totalWorkingDays + " emp Hr:" +empHrs);
 
 	}
+		int totalEmpWage= totalEmpHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Emp Total Wage: " +totalEmpWage);
 
-}}
+}
+
+}
+
+
