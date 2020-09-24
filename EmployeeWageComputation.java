c02af2b51facf31a39257b7e01e3450ef675a52a
kvishwasrujan <roohithbandi00@gmail.com>
2020-09-24 20:34:59 +0530

    Added:Calculated Daily Employee Wage fuctionality added

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,15 +1,21 @@
 package empWageBuilder;
 
-public class EmpWageBuilderUC1 {
+public class EmpWageBuilderUC2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HOUR = 20;
+		int empHrs = 0;
+		int empWage = 0;
 		double empCheck = Math.floor(Math.random()*10)%2;
 		if(empCheck == IS_FULL_TIME)
-			System.out.println("Employee is Present");
+			empHrs = 8;
 		else
-			System.out.println("Employee is Absent");
+			empHrs= 0;
+		empWage = empHrs*EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage: "+ empWage);
+
 	}
 
 }
